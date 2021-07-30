e8dd8670a4c34ac892e7ac995f031bf301c429d9
Yubaraj Rai <connectyubaraj@gmail.com>
2021-07-30 16:20:31 +0545

    Init

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <!-- ========== PAGE TITLE ========== -->
+    <title>Website is under construction</title>
+    <!-- ========== META TAGS ========== -->
+    <meta name="description" content="Website is under construction">
+    <meta name="keywords" content="Coming soon, Under construction, Website under construction">
+    <meta charset="utf-8">
+    <meta name="author" content="Tech101">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- ========== VIEWPORT META ========== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <!-- ========== FAVICON & APPLE ICONS ========== -->
+    <link rel="shortcut icon" href="favicon.ico"> 
+    <!-- ========== GOOGLE FONTS ========== -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,100" rel='stylesheet' type='text/css'> 
+    <!-- ========== MINIFIED VENDOR CSS ========== -->
+    <link rel="stylesheet" href="styles/vendor.css">
+    <link rel="stylesheet" href="styles/main.css">
+    
+    <!-- ========== MODERNIZR ========== -->
+    <script src="scripts/vendor/modernizr.js"></script>
+</head> 
+
+<body class="light-header animsition">
+    <div class="" id="fullpage">
+        <!-- ========== PARTICLES DIV ========== -->
+            <section class="particles-div section content-section" id="home">
+                <div class="content">
+                    <div class="container outer">
+                        <div class="row inner">
+                            <div class="col-md-12 centered">
+
+                                <!-- ========== Typer Headline ========== -->
+                                <h1 class="headline">We believe in <span id="typer" class="typer" data-typer-targets="Perfection, Creation, Impression"></span>. <small>Website Under Construction.</small></h1>
+<div align="center"> 
+<p>&nbsp;</p>
+Powered by <a href="https://www.tech101.co.uk" target="_blank" rel="noopener">Tech101</a></blockquote>
+
+</div>
+                            </div> <!-- end col-md-12 -->
+ <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Ad for Tech101 Hosting account Default Page -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3473320487765930"
+     data-ad-slot="3432470056"
+     data-ad-format="link"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+                        </div> <!-- end row -->
+                    </div> <!-- end container -->
+
+                </div> <!-- end content -->
+            </section> <!-- end section -->
+    </div>
+    <!-- end fullpage -->  
+    
+    <script src="scripts/vendor.js"></script>
+    <!-- ========== MINIFIED PLUGINS JS ========== -->
+    <script src="scripts/plugins.js"></script>
+    <script src="scripts/main.js"></script>
+    <script src="scripts/init-particles.js"></script>
+</body> 
+</html>
